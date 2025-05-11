85a108720ea1509f648655e5dd345f7e4bd0ffc7
Emidsm <emidsm2005@gmail.com>
2025-05-11 15:05:13 -0600

    Modificaciones para estilo responsivo completo en HomeWindow.

---
+++ GymFlash/View/WidthToHeightConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace GymFlash.View
+{
+    public class WidthToHeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double width)
+            {
+                double ratio = 9.0 / 16; // Default
+                if (parameter is string paramStr && double.TryParse(paramStr, out double customRatio))
+                {
+                    ratio = customRatio;
+                }
+                return width * ratio;
+            }
+            return 300;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
