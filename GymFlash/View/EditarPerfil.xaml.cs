8da29e8fb1a5a68966dd39ed8e57f8d785a7b279
LAPTOP-2DJI3GM9\abisa <abisaicordovadevora9186@gmail.com>
2025-05-11 18:24:38 -0600

    agregacion de la pantalla de editar perfil, y cambio en la logica para el boton de editar perfil en Perfil de usuario ademas de cambio en la logica de PerfilUsuario.xaml.cs

---
+++ GymFlash/View/EditarPerfil.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GymFlash.View
+{
+    public partial class EditarPerfil : Window
+    {
+        public EditarPerfil()
+        {
+            InitializeComponent();
+        }
+
+        private void Guardar_Click(object sender, RoutedEventArgs e)
+        {
+            // Aquí podrías guardar la información, por ahora solo mostramos un mensaje.
+            MessageBox.Show("Perfil actualizado correctamente.", "Éxito", MessageBoxButton.OK, MessageBoxImage.Information);
+            this.Close();
+        }
+
+        private void Cancelar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
--- GymFlash/View/PerfilUsuario.xaml.cs
+++ GymFlash/View/PerfilUsuario.xaml.cs
@@ -1,27 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace GymFlash.View
 {
-    /// <summary>
-    /// Lógica de interacción para PerfilUsuario.xaml
-    /// </summary>
     public partial class PerfilUsuario : Window
     {
         public PerfilUsuario()
         {
             InitializeComponent();
+        }
+
+        private void EditarPerfil_Click(object sender, RoutedEventArgs e)
+        {
+            EditarPerfil ventanaEditar = new EditarPerfil();
+            ventanaEditar.ShowDialog();
         }
     }
 }
