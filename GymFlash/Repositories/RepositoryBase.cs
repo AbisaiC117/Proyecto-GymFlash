ec17d0361638b6a51aca9cf517ab4af5bce55803
emidsm <emidsm2005@gmail.com>
2025-05-19 17:00:23 -0600

    RepositoryBase.cs

---
+++ GymFlash/Repositories/RepositoryBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace GymFlash.Repositories
+{
+    public abstract class RepositoryBase
+    {
+        private readonly String _connectionString;
+
+        public RepositoryBase()
+        {
+            _connectionString =
+                "Server = DESKTOP-B68VNIO\\VSGESTION;" +
+                "Database=GymFlashDB;" +
+                "Integrated Security = true";
+        }
+
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
